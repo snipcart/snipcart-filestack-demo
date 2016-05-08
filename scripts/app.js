d8cc090e280745477537806c3043d6d8f40c3f21
Charles Ouellet <charles.ouellet@gmail.com>
2016-05-08 17:17:55 -0400

    Used a theme on Start Bootstrap to make it look better.

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+filepicker.setKey('AnHcmQdOGT1yzcIx00fmfz');
+
+Snipcart.subscribe('cart.ready', function() {
+    Snipcart.api.configure('show_cart_automatically', false);
+});
+
+$(function() {
+    $('.add-file').click(function() {
+        filepicker.pick({
+            mimetype: 'image/*',
+            services: ['COMPUTER']
+        }, function (blob) {
+            var baseUrl = "https://process.filestackapi.com/AnHcmQdOGT1yzcIx00fmfz"
+            console.log(blob);
+            var screenshotUrl = baseUrl + "/urlscreenshot/";
+            var url = screenshotUrl + baseUrl + "/ascii/" + blob.url;
+            
+            Snipcart.api.items.add({
+                id: "FilestackImage",
+                name: 'Custom ASCII poster',
+                url: "/snipcart-filestack-demo",
+                image: url,
+                price: 30,
+                customFields: [{
+                    type: 'hidden',
+                    name: 'Image URL',
+                    vaue: url
+                }]
+            }).then(function() {
+                Snipcart.api.modal.show();
+            });
+        });
+    });
+});
