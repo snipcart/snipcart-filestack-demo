b2940ae88cca468c9e86b5b8fe901f2dbd5eb3d0
Charles Ouellet <charles.ouellet@gmail.com>
2016-05-09 15:12:59 -0400

    Some fixes

--- scripts/app.js
+++ scripts/app.js
@@ -4,31 +4,38 @@ Snipcart.subscribe('cart.ready', function() {
     Snipcart.api.configure('show_cart_automatically', false);
 });
 
+var imageUrl;
+
 $(function() {
-    $('.add-file').click(function() {
+    $('#add-file').click(function() {
         filepicker.pick({
             mimetype: 'image/*',
             services: ['COMPUTER']
         }, function (blob) {
-            var baseUrl = "https://process.filestackapi.com/AnHcmQdOGT1yzcIx00fmfz"
-            console.log(blob);
-            var screenshotUrl = baseUrl + "/urlscreenshot/";
-            var url = screenshotUrl + baseUrl + "/ascii/" + blob.url;
+            var baseUrl = "https://process.filestackapi.com/AnHcmQdOGT1yzcIx00fmfz";
+            var asciiUrl = baseUrl + "/ascii/" + blob.url;
+            imageUrl = baseUrl + "/urlscreenshot/" + asciiUrl;
             
-            Snipcart.api.items.add({
-                id: "FilestackImage",
-                name: 'Custom ASCII poster',
-                url: "/snipcart-filestack-demo",
-                image: url,
-                price: 30,
-                customFields: [{
-                    type: 'hidden',
-                    name: 'Image URL',
-                    value: url
-                }]
-            }).then(function() {
-                Snipcart.api.modal.show();
-            });
+            $('#preview').attr('src', imageUrl);
+            $('#add-file').hide();
+            $('#add-to-cart').show();
         });
+    });
+    
+    $('#add-to-cart').click(function() {
+        Snipcart.api.items.add({
+            id: "FilestackImage",
+            name: 'Custom ASCII poster',
+            url: "/snipcart-filestack-demo",
+            image: imageUrl,
+            price: 30,
+            customFields: [{
+                type: 'hidden',
+                name: 'Image URL',
+                value:imageUrl
+            }]
+        }).then(function() {
+            Snipcart.api.modal.show();
+        }); 
     });
 });
